systemd/systemd-netlogd

commit 205dbe6191911f5dbbdc7f7460c9de13807f9004

Actually respect UseSysLogStructuredData= and UseSysLogMsgId= settings

src/netlog/netlog-manager.c
@@ -198,8 +198,8 @@ static int manager_read_journal_input(Manager *m) {
                                        message, hostname,
                                        pid,
                                        r >= 0 ? &tv : NULL,
-                                       structured_data,
-                                       msgid);
+                                       m->structured_data ? structured_data : NULL,
+                                       m->syslog_msgid ? msgid : NULL);
 }
 
 static int update_cursor_state(Manager *m) {

commit 191ad7e362673e79357320d39ecd2a1ed66cd466

Do not override StructuredData=
In case StructuredData= is set, actually use it and do not override it
with the SYSLOG_STRUCTURED_DATA= parsed data.

Fixes: 205dbe61 ("Actually respect UseSysLogStructuredData= and UseSysLogMsgId= settings")

src/netlog/netlog-manager.c
@@ -198,7 +198,7 @@ static int manager_read_journal_input(Manager *m) {
                                        message, hostname,
                                        pid,
                                        r >= 0 ? &tv : NULL,
-                                       m->structured_data ? structured_data : NULL,
+                                       structured_data,
                                        m->syslog_msgid ? msgid : NULL);
 }
 

src/netlog/netlog-protocol.c
@@ -150,7 +150,7 @@ int format_rfc5424(Manager *m,
         /* Eighth: [structured-data] */
         if (m->structured_data)
                 IOVEC_SET_STRING(iov[n++], m->structured_data);
-        else if (syslog_structured_data)
+        else if (m->syslog_structured_data && syslog_structured_data)
                 IOVEC_SET_STRING(iov[n++], syslog_structured_data);
         else
                 IOVEC_SET_STRING(iov[n++], RFC_5424_NILVALUE);